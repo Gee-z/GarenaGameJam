d043be106c243cfb3ef71ab11d34317e52cc1303
GeezNuts <157205180+Gee-z@users.noreply.github.com>
2026-02-08 10:15:34 +0700

    scene connected

--- Assets/Script/UI/ButtonHover.cs
+++ Assets/Script/UI/ButtonHover.cs
@@ -22,7 +22,7 @@ public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     public void StartGame()
     {
         AudioManager.Instance.PlaySFX(0);
-        SceneManager.LoadScene("GameScene"); 
+        // SceneManager.LoadScene("GameScene"); 
     }
 
     public void QuitGame()
